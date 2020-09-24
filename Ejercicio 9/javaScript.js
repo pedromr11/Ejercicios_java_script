a74504761593a64e6af671ec849c95adb428250c
Pedro Muñoz <pedromurivas11@gmail.com>
2020-09-24 10:01:46 +0200

    Ejercicio 9 de java script de la introducción

---
+++ Ejercicio 9/javaScript.js
@@ -0,0 +1,20 @@
+
+//Función con bucle while
+function numeros(){
+    var i = 1;
+    while(i <= 35){
+        if(i%2 != 0){
+            console.log(i);
+        }
+        i++;
+    }
+}
+
+//Función con bucle for
+function numeros2(){
+    for (let i = 1; i <= 35; i++){
+        if(i%2 != 0){
+            console.log(i);
+        }
+    }
+}
